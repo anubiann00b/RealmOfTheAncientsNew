352eb5351b232445aac417d7778f89374190551e
Shreyas <skraman1999@gmail.com>
2017-01-21 01:25:20 -0800

    Add initial project skeleton

---
+++ desktop/src/me/shreyasr/ancients/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.shreyasr.ancients.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.shreyasr.ancients.AncientsGame;
+
+public class DesktopLauncher {
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new AncientsGame(), config);
+    }
+}
